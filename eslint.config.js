c05720036e0d8e319a36150d766bc2dcdfc87882
fengkx <liangkx8237@gmail.com>
2023-03-01 04:24:31 +0800

    chore: pnpm workspace ts eslint

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+module.exports = [
+  {
+    languageOptions: {
+      ecmaVersion: "2022",
+      sourceType: "module",
+      parser: "@typescript-eslint/parser",
+    },
+    plugins: ["@typescript-eslint"],
+    rules: {},
+  },
+];
