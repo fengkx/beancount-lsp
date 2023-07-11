4dd4ece4fedd4165dcdf1cc184eb1dc71521c0e4
fengkx <liangkx8237@gmail.com>
2023-07-11 11:08:02 +0800

    wip

---
+++ packages/lsp/src/queries/narration.scm
@@ -0,0 +1,1 @@
+(transaction narration: (string) @narration)
--- packages/lsp/t.js
+++ packages/lsp/t.js
@@ -2,6 +2,8 @@ const Parser = require("tree-sitter");
 const Beancount = require("tree-sitter-beancount");
 const fs = require("fs");
 
+const { Query, QueryCursor } = Parser;
+
 const parser = new Parser();
 parser.setLanguage(Beancount);
 const r = parser.parse(
@@ -12,3 +14,7 @@ const r = parser.parse(
 );
 
 console.log(r.rootNode.toString());
+const query = new Query(Beancount, "((date) @date)");
+
+const matches = query.matches(r.rootNode);
+console.log(matches[0]);
