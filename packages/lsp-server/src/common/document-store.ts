08bd7b028f225fb9927dc36fa7f5aa6d076ba943
fengkx <liangkx8237@gmail.com>
2024-02-02 16:39:33 +0800

    refactor: document store

---
+++ packages/lsp-server/src/common/document-store.ts
@@ -0,0 +1,60 @@
+import { LRUMapWithDelete as LRUMap } from "mnemonist";
+import { Connection, TextDocuments } from "vscode-languageserver";
+import { TextDocument } from "vscode-languageserver-textdocument";
+import { Utils as UriUtils, URI } from 'vscode-uri'
+
+
+
+export class DocumentStore extends TextDocuments<TextDocument> {
+    constructor(private readonly _connection: Connection) {
+        super({
+            create: TextDocument.create,
+            update: TextDocument.update,
+        });
+        this.listen(_connection);
+    }
+    private readonly _documentsCache = new LRUMap<string, TextDocument>(200)
+
+    async retrieve(uri: string): Promise<TextDocument> {
+        const result = this.get(uri);
+        return result!;
+
+        // let cached = this._documentsCache.get(uri);
+        // if (!cached) {
+        //     cached = this._requestDocument(uri)
+        //     result = undefined
+        // }
+    }
+
+    removeFile(uri: string) {
+        return this._documentsCache.delete(uri);
+    }
+
+    private async getConfiguration() {
+        const config = await this._connection.workspace.getConfiguration({ section: LANGUAGE_ID });
+        console.info(config);
+        return config
+    }
+
+    public async getMainBeanFileUri(): Promise<string | null> {
+        const config = await this.getConfiguration();
+        const workspace = await this._connection!.workspace.getWorkspaceFolders();
+
+        if (!workspace) {
+            // just open a file
+            return null
+        }
+
+        if (workspace && !config.mainBeanFile) {
+            this._connection!.window.showWarningMessage(`Using default 'main.bean' as mainBeanFile, You should configure 'beancount.mainBeanFile'`)
+        }
+        const rootUri = workspace[0].uri;
+
+        const mainAbsPath = UriUtils.joinPath(URI.parse(rootUri), config.mainBeanFile ?? 'main.bean');
+
+
+        return mainAbsPath.toString();
+
+    }
+
+}
--- packages/lsp-server/src/common/startServer.ts
+++ packages/lsp-server/src/common/startServer.ts
@@ -1,7 +1,7 @@
 import { Connection, InitializeParams, InitializeResult, TextDocumentSyncKind } from "vscode-languageserver";
 import { getParser } from "@bean-lsp/shared";
 import { Feature } from "./features/types";
-export function startServer(connection: Connection) {
+export function startServer(connection: Connection, mainBeanFile) {
     console.log = connection.console.log.bind(connection.console);
     console.warn = connection.console.warn.bind(connection.console);
     console.error = connection.console.error.bind(connection.console);
