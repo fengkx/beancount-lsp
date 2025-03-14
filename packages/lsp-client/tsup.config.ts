9225c6111738f30060fe71f34b2afa5a90b1d85a
fengkx <liangkx8237@gmail.com>
2025-03-14 20:04:41 +0800

    refactor: simplify tsup configuration by introducing common options
    - Extracted common configuration options into a shared object for better maintainability.
    - Updated node and browser configurations to utilize the common options, ensuring consistency across builds.

--- packages/lsp-client/tsup.config.ts
+++ packages/lsp-client/tsup.config.ts
@@ -1,37 +1,34 @@
 import { nodeModulesPolyfillPlugin } from 'esbuild-plugins-node-modules-polyfill';
-import { defineConfig } from 'tsup';
+import { defineConfig, Options } from 'tsup';
+
+// Common configuration options
+const commonConfig: Partial<Options> = {
+	outDir: 'dist',
+	sourcemap: true,
+	target: 'es2022',
+	loader: {
+		'.wasm': 'binary',
+		'.scm': 'text',
+	},
+	external: ['vscode'],
+	noExternal: ['@bean-lsp/shared', 'vscode-languageclient', 'vscode-uri'],
+};
 
 const nodeConfig = defineConfig({
+	...commonConfig,
 	entry: {
 		'node/extension': 'src/node/extension.ts',
 	},
-	outDir: 'dist',
-	sourcemap: true,
 	clean: true,
-	target: 'es2022',
-	loader: {
-		'.wasm': 'binary',
-		'.scm': 'text',
-	},
-	external: ['vscode'],
-	noExternal: ['@bean-lsp/shared', 'vscode-languageclient', 'vscode-uri'],
 });
 
 const browserConfig = defineConfig({
+	...commonConfig,
 	entry: {
 		'browser/extension': 'src/browser/extension.ts',
 	},
-	outDir: 'dist',
-	sourcemap: true,
 	platform: 'browser',
 	clean: false, // Don't clean twice
-	target: 'es2022',
-	loader: {
-		'.wasm': 'binary',
-		'.scm': 'text',
-	},
-	external: ['vscode'],
-	noExternal: ['@bean-lsp/shared', 'vscode-languageclient', 'vscode-uri'],
 	esbuildPlugins: [nodeModulesPolyfillPlugin(
 		{
 			modules: {
