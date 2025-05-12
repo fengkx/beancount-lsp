b77640d7d90c0567063d8237ebf0215209a39cc5
fengkx <liangkx8237@gmail.com>
2025-05-12 21:03:25 +0800

    feat: add support for cjk account name

--- packages/lsp-client/scripts/make-word-pattern.js
+++ packages/lsp-client/scripts/make-word-pattern.js
@@ -4,19 +4,23 @@ const { modify, applyEdits } = require('jsonc-parser');
 const { capture, atomic, either } = require('compose-regexp');
 
 // https://flems.io/#0=N4IgZglgNgpgziAXAbVAOwIYFsZJAOgAsAXLKEAGhAGMB7NYmBvEAXwvW10QICsEqdBk2J4IWAA60ATsQAEwDMVpYI1CnOoYJxAK7SYGgEa1daACYbqhDNIDKMYhrBQMAczga4MAI66m1IZycLpgkAAeGjAQxIQw0qxyYNIqcgDkdJK03gC0Bm4w4RIAAgAM+ABs+KVpADpo9UJw8mi6WEbxcgC8mtp6BgAUSipqAwD0OQD8tbXmAFQz+NOzANRyc2MAlJsA3PWN9M1yGNR0ZvI9Wjr6MEPKqtTjAwPIAII5AFoAum+fGDkAL1KOQAnDMcl85psBigZhJgAAZXSsH5wxGseEAOVY4MhmxWmy2m32aCa8kyWFo5hiAE9ur1roNhg9xr9vmzgSC6rV8AB9XHAUoUABMwtYHNBkKJewapMO8mI7npV36t2ZozGAwAxMgZgB3XEE6UkslyKAQNAAa2VfRudxGjzGMwAerragbahCVsbZaaTmcGAAFWwXBmq+0ssZsn7-AGo92GxBEk3yuRoGRYDBQADqMnMNsZavuGuQsa+3u2MoOaDgtFg+CgtDcA1a7Xiu2rtfrjeb-tMDA7cprdZgDabAwpVNpxKHXdHPYGircg6aI7HzfNVpXhzXC7752Dsm3w+74-T0kzObzM878gMISgoeisXiLbaHWkGn3DCsKkp1OIGkNCXDRN0tL9Tn7YhDycNMMyzXNpHMGdV1PZt710R8Z0oEBvFgahiAgQ48AAZkQUo2A4EBMBwPB8GoOABBoehGGYHg2C+VggA
-const number = capture(atomic(/-?\d*\.?\d+ */));
+const number = capture(atomic(/-?\d*\.?\d+ */u));
 
-const account = capture(atomic(/(([A-Z][A-Za-z0-9\-]*)(:[\p{Lu}][\p{L}p{N}\-]*)+)/));
+const account = capture(
+	atomic(
+		/(([A-Z][A-Za-z0-9\-]*)(:[\p{Lu}\u{4E00}-\u{9FFF}\u{3400}-\u{4DBF}\u{F900}-\u{FAFF}\u{20000}-\u{2A6DF}\u{2A700}-\u{2B73F}\u{2B740}-\u{2B81F}\u{2B820}-\u{2CEAF}\u{2F800}-\u{2FA1F}\u{3040}-\u{309F}\u{30A0}-\u{30FF}\u{AC00}-\u{D7AF}][\p{L}\p{N}\u{4E00}-\u{9FFF}\u{3400}-\u{4DBF}\u{F900}-\u{FAFF}\u{20000}-\u{2A6DF}\u{2A700}-\u{2B73F}\u{2B740}-\u{2B81F}\u{2B820}-\u{2CEAF}\u{2F800}-\u{2FA1F}\u{3040}-\u{309F}\u{30A0}-\u{30FF}\u{AC00}-\u{D7AF}\-]*))+/u,
+	),
+);
 
-const commodity = capture(atomic(/[A-Z][A-Z0-9'\._\-]{0,22}[A-Z0-9]*/));
+const commodity = capture(atomic(/[A-Z][A-Z0-9'\._\-]{0,22}[A-Z0-9]*/u));
 
-const tag = capture(atomic(/(#[\w\-]+)/));
+const tag = capture(atomic(/(#[\w\-]+)/u));
 
-const link = capture(atomic(/\^[\w\-]+/));
+const link = capture(atomic(/\^[\w\-]+/u));
 
-const accountPart = capture(atomic(/[A-Z][a-z][\w\-]+:/));
+const accountPart = capture(atomic(/[A-Z][a-z][\w\-]+:/u));
 
-const normalWord = capture(atomic(/[a-z]+/));
+const normalWord = capture(atomic(/[a-z]+/u));
 
 const regex = either(number, account, commodity, tag, link, accountPart, normalWord);
 
